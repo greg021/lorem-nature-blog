e89a53f10bb87de7ee8ee33c3214c3cbe2fb5c8a
greg021 <greg021@protonmail.com>
2021-02-08 00:17:29 +0530

    Created paginated pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,48 +1,48 @@
-// exports.createPages = async function ({ actions, graphql }) {
-//   const data = await graphql`
-//     query {
-//       allMdx(sort: { fields: frontmatter___date, order: DESC }) {
-//         edges {
-//           node {
-//             frontmatter {
-//               slug
-//             }
-//             id
-//           }
-//         }
-//       }
-//     }
-//   `;
+exports.createPages = async function ({ actions, graphql }) {
+  const postTemplate = require.resolve("./src/templates/allPosts.js");
+  const data = await graphql(`
+    query {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
+            id
+          }
+        }
+      }
+    }
+  `);
 
-//   //create paginated pages for posts
+  //create paginated pages for posts
 
-//   const postPerPage = 3;
+  const postPerPage = 3;
+  const numPages = Math.ceil(data.data.allMdx.edges.length / postPerPage);
 
-//   const numPages = Math.ceil(data.allMdx.edges.length / postPerPage);
+  Array.from({ length: numPages }).forEach((_, i) => {
+    actions.createPage({
+      path: i === 0 ? `/` : `/${i + 1}`,
+      component: postTemplate,
+      context: {
+        limit: postPerPage,
+        skip: i * postPerPage,
+        numPages,
+        currentPage: i + 1,
+      },
+    });
+  });
 
-//   Array.from({ length: numPages }).forEach((_, i) => {
-//     actions.createPages({
-//       path: i === 0 ? `/` : `/${i + 1}`,
-//       component: require.resolve("./src/templates/allPosts.js"),
-//       context: {
-//         limit: postPerPage,
-//         skip: i * postPerPage,
-//         numPages,
-//         currentPage: i + 1,
-//       },
-//     });
-//   });
+  // //create single blog post
 
-//   //create single blog post
+  // data.allMdx.edges.forEach((edge) => {
+  //   const slug = edge.node.frontmatter.slug;
+  //   const id = edge.node.id;
 
-//   data.allMdx.edges.forEach((edge) => {
-//     const slug = edge.node.frontmatter.slug;
-//     const id = edge.node.id;
-
-//     actions.createPages({
-//       path: slug,
-//       component: require.resolve("./src/templates.singlePost.js"),
-//       context: { id },
-//     });
-//   });
-// };
+  //   actions.createPages({
+  //     path: slug,
+  //     component: require.resolve("./src/templates.singlePost.js"),
+  //     context: { id },
+  //   });
+  // });
+};
---
+++ src/templates/allPosts.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { graphql } from "gatsby";
+import {
+  Container,
+  Content,
+  ContentCard,
+  FeatureImage,
+  Pagination,
+} from "../components";
+import { H1, P } from "../elements";
+
+const allPosts = ({ pageContext, data }) => {
+  const { currentPage, numPages } = pageContext;
+  const isFirst = currentPage === 1;
+  const isLast = currentPage === numPages;
+  const prevPage = currentPage - 1 === 1 ? "/" : `/${currentPage - 1}`;
+  const nextPage = `/${currentPage + 1}`;
+  const posts = data.allMdx.edges;
+
+  return (
+    <Container>
+      <FeatureImage />
+      <Content>
+        <H1 textAlign="center" margin="0 0 1rem 0">
+          Welcome to The Blog Of Vilukihnejufetrewd
+        </H1>
+        <P color="dark2" text-align="center">
+          Lorem error sit voluptatem accusantium doloremque laudantium, totam
+          rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+          architecto beatae vitae dicta sunt explicabo.
+        </P>
+        {posts.map((post) => {
+          return (
+            <ContentCard
+              key={post.node.frontmatter.slug}
+              date={post.node.frontmatter.date}
+              title={post.node.frontmatter.title}
+              excerpt={post.node.frontmatter.excerpt}
+              slug={post.node.frontmatter.slug}
+            />
+          );
+        })}
+      </Content>
+      <Pagination
+        isFirst={isFirst}
+        isLast={isLast}
+        prevPage={prevPage}
+        nextPage={nextPage}
+      />
+    </Container>
+  );
+};
+
+export default allPosts;
+
+export const pageQuery = graphql`
+  query allPostsQuery($skip: Int!, $limit: Int!) {
+    allMdx(
+      sort: { fields: frontmatter___date, order: DESC }
+      skip: $skip
+      limit: $limit
+    ) {
+      edges {
+        node {
+          frontmatter {
+            slug
+            date(formatString: "MMMM DD, YYYY")
+            title
+            excerpt
+          }
+        }
+      }
+    }
+  }
+`;
