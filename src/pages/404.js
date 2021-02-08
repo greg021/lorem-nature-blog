cd4750200cc3d4ad52f28c6e69d355dcc822b365
greg021 <greg021@protonmail.com>
2021-02-08 11:32:03 +0530

    Added 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { graphql } from "gatsby";
+import { Container, Content, FeatureImage } from "../components";
+import { H1 } from "../elements";
+
+const notFound = ({ data }) => {
+  const featureImage = data.imageSharp.fixed;
+
+  return (
+    <Container>
+      <FeatureImage fixed={featureImage} />
+      <Content>
+        <H1 textAlign="center" margin="0 0 1rem 0">
+          {" "}
+          Uh-oh ! The page you requested couldn't be found.
+        </H1>
+      </Content>
+    </Container>
+  );
+};
+
+export default notFound;
+
+export const notFoundQuery = graphql`
+  query NotFoundQuery {
+    imageSharp(fixed: { originalName: { eq: "404.jpg" } }) {
+      fixed {
+        ...GatsbyImageSharpFixed
+      }
+    }
+  }
+`;
